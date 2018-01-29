f1be198412970f3e4d6f2f31161ef223cccd7169
不会抽烟 <877520617@qq.com>
2018-01-29 10:53:23 +0800

    Initial Commit

---
+++ LiuqsChooseSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiuqsChooseSlider
+//
+//  Created by liuqs on 2018/1/29.
+//  Copyright © 2018年 liuqs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
